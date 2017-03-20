5e80a470a5a3cdaf424054a48bd355fe71c92ea1
cp2379 <cp2379@nyu.edu>
2017-03-20 14:44:29 -0400

    changes

--- src/main/java/edu/nyu/oop/CppAST.java
+++ src/main/java/edu/nyu/oop/CppAST.java
@@ -1,29 +1,12 @@
 package edu.nyu.oop;
 
-import java.io.*;
-import java.util.*;
-
-import edu.nyu.oop.util.XtcProps;
-import org.slf4j.Logger;
 import xtc.lang.CPrinter;
-import xtc.tree.Node;
 import xtc.tree.GNode;
 import xtc.tree.Printer;
 import xtc.lang.CFeatureExtractor;
-import xtc.tree.Visitor;
-import edu.nyu.oop.util.NodeUtil;
-import xtc.tree.Visitor;
 import xtc.util.Runtime;
-import xtc.tree.Location;
 import xtc.util.SymbolTable;
 
-
-import java.io.File;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.*;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 /**
  * Created by charlottephillips on 09/03/17.
@@ -35,20 +18,18 @@ public class CppAST extends CPrinter{
     CFeatureExtractor cextractor = new CFeatureExtractor(runtime);
 
 
-
     public CppAST(Printer printer) {
         super(printer);
     }
-
 
     public void nodeParser(GNode n){
         // create new symbol table for each node, where the node will be set as the root's scope name
         // the symbol table will automatically enter and extit the scope in during traversal
         SymbolTable symbolTable = new SymbolTable(n.toString());
-            cextractor.visit(n);
-            cextractor.process(n,symbolTable);
-            String cID = symbolTable.freshCId(n.toString()); // create a C identifier, where n is the base name
-            String nodeName = n.getName();
-            nodeName = cID;
+        cextractor.visit(n);
+        cextractor.process(n,symbolTable);
+        String cID = symbolTable.freshCId(n.toString()); // create a C identifier, where n is the base name
+        String nodeName = n.getName();
+        nodeName = cID;
     }
 }
