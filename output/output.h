3eedfe133afee4e74e968b345b89354305155102
rishabhranawat <rishabhranawat12345@gmail.com>
2017-03-25 16:17:23 -0400

    ran output

--- output/output.h
+++ output/output.h
@@ -14,36 +14,17 @@ struct __A
     __A()();
     static Class __class();
     static __A()_VT __vtable;
-    void setA(String)
-    void printOther(A)
     String toString(A)
 };
-struct __B1;
-struct __B1_VT;
-struct __B1
+struct __B;
+struct __B_VT;
+struct __B
 {
-    __B1_VT* __vptr;
-    __B1()();
+    __B_VT* __vptr;
+    __B()();
     static Class __class();
-    static __B1()_VT __vtable;
-};
-struct __B2;
-struct __B2_VT;
-struct __B2
-{
-    __B2_VT* __vptr;
-    __B2()();
-    static Class __class();
-    static __B2()_VT __vtable;
-};
-struct __C;
-struct __C_VT;
-struct __C
-{
-    __C_VT* __vptr;
-    __C()();
-    static Class __class();
-    static __C()_VT __vtable;
+    static __B()_VT __vtable;
+    String toString(B)
 };
 struct __A_VT
 {
@@ -61,51 +42,19 @@ struct __A_VT
     }
 
 
-    struct __B1_VT
+    struct __B_VT
 {
     Class __is_a;
-    int32_t (*hashCode)(B1);
-    bool (*equals)(B1, Object);
-    Class (*getClass)(B1);
-    String (*toString)(B1);
-    __B1_VT()
-        : __is_a(__B1::__class()),
-          hashCode((int_32(*)( B1 )) &__Object::hashCode),
-          equals((bool(*)( B1, Object )) &__Object::equals),
-          toString((String(*)( B1 )) &__Object::toString),
-          getClass((Class(*)( B1 )) &__Object::getClass),
-    }
-
-
-    struct __B2_VT
-{
-    Class __is_a;
-    int32_t (*hashCode)(B2);
-    bool (*equals)(B2, Object);
-    Class (*getClass)(B2);
-    String (*toString)(B2);
-    __B2_VT()
-        : __is_a(__B2::__class()),
-          hashCode((int_32(*)( B2 )) &__Object::hashCode),
-          equals((bool(*)( B2, Object )) &__Object::equals),
-          toString((String(*)( B2 )) &__Object::toString),
-          getClass((Class(*)( B2 )) &__Object::getClass),
-    }
-
-
-    struct __C_VT
-{
-    Class __is_a;
-    int32_t (*hashCode)(C);
-    bool (*equals)(C, Object);
-    Class (*getClass)(C);
-    String (*toString)(C);
-    __C_VT()
-        : __is_a(__C::__class()),
-          hashCode((int_32(*)( C )) &__Object::hashCode),
-          equals((bool(*)( C, Object )) &__Object::equals),
-          toString((String(*)( C )) &__Object::toString),
-          getClass((Class(*)( C )) &__Object::getClass),
+    int32_t (*hashCode)(B);
+    bool (*equals)(B, Object);
+    Class (*getClass)(B);
+    String (*toString)(B);
+    __B_VT()
+        : __is_a(__B::__class()),
+          toString(__B::toString),
+          hashCode((int_32(*)( B )) &__Object::hashCode),
+          equals((bool(*)( B, Object )) &__Object::equals),
+          getClass((Class(*)( B )) &__Object::getClass),
     }
 
 
