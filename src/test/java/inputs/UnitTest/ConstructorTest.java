c489f2792c735f6a02e5ef49fde24c39428f15cc
Stephanie McAleer <sm6405@nyu.edu>
2017-04-19 18:31:16 -0400

    add unit test class

---
+++ src/test/java/inputs/UnitTest/ConstructorTest.java
@@ -0,0 +1,38 @@
+package inputs.UnitTest;
+
+/**
+ * Created by stephaniemcaleer on 4/19/17.
+ */
+class A {
+
+    public A(String s) {};
+
+    public A() {};
+}
+
+class B {
+
+}
+
+class C {
+    public C() {};
+
+    public C(int i) {};
+
+    public C(double d) {};
+
+}
+
+public class ConstructorTest {
+    public static void main(String[] args) {
+    A a1 = new A("test");
+    A a = new A();
+
+    B b = new B();
+
+    C c = new C(3);
+    C c2 = new C(5.4);
+    C c3 = new C();
+
+    }
+}
