1bc5cd62318d8013db28be140b7244c33f00e027
svalous <bluepower912@gmail.com>
2017-04-19 19:34:47 -0400

    working on tests

--- src/test/java/edu/nyu/oop/ConstructorTest.java
+++ src/test/java/edu/nyu/oop/ConstructorTest.java
@@ -13,6 +13,10 @@ import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -20,19 +24,47 @@ import java.util.List;
  */
 public class ConstructorTest {
 
-    @BeforeClass
-    public static void beforeClass() {
+    private static GNode n;
 
+    @BeforeClass
+    public static void beforeClassTest() {
+        n = (GNode) XtcTestUtils.loadTestFile(("src/test/java/inputs/UnitTest/ConstructorTest.java"));
     }
 
     @Before
     public void beforeTest() {
+        System.out.println("Executing ConstructorTest");
+        try {
+                CreateHeaderDataLayout head = new CreateHeaderDataLayout(n);
+                jppPrinter jpp = new jppPrinter(n);
+            } catch (IOException e) {
 
+            }
     }
 
 
     @Test
     public void testConstructor() {
+        try {
+            // setup
+            File f = new File("output/output.cpp");
+            ArrayList<String> lines = new ArrayList<String>();
+            Scanner s = new Scanner(f);
 
+            // read lines
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                lines.add(line);
+            }
+
+            // asserts
+            // lines are line + 1
+            // TODO: Add dummy, change line numbers
+            assertEquals("A::__init(new__A(), (A __this , String s)) {", lines.get(12));    // vtable implementation for A(String s)
+            assertEquals("A::__init(new__A(), (A __this ) {", lines.get(14));    // vtable implementation for A()
+            s.close();
+        } catch (FileNotFoundException e) {
+
+        }
    }
 }
