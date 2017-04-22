dcdabe27cc7e9943958ba682e92f95af88a741c9
rishabhranawat <rishabhranawat12345@gmail.com>
2017-04-22 15:05:48 -0400

    details

--- src/main/java/edu/nyu/oop/DependencyInheritance.java
+++ src/main/java/edu/nyu/oop/DependencyInheritance.java
@@ -5,22 +5,38 @@ import xtc.tree.Node;
 import xtc.tree.Visitor;
 import xtc.type.JavaAST;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by rishabh on 22/04/17.
  */
+
+class TreeNode{
+    String className;
+    String packageName;
+    TreeNode parent;
+    List<TreeNode> extendsNodes = new ArrayList<>();
+    Node ast;
+
+    public TreeNode(String packageName, String className, Node ast){
+        this.className = className;
+        this.packageName = packageName;
+        this.ast = ast;
+    }
+
+    public String toString(){
+        return this.className+" "+this.packageName+" ";
+    }
+}
 public class DependencyInheritance extends Visitor {
 
+    public TreeNode inheritanceTree = new TreeNode("Java", "Object", null);
     public List<GNode> headerAsts;
 
     public void getDependencyInheritance(Node n){
         CreateHeaderAST visitor = new CreateHeaderAST();
         this.headerAsts = visitor.getHeaderAsts(n);
-
-//        for(GNode dependencyAstNode: headerAsts){
-//            super.dispatch(dependencyAstNode);
-//        }
         simulateInheritance();
     }
 
@@ -59,14 +75,30 @@ public class DependencyInheritance extends Visitor {
         }
     }
 
+    public TreeNode createTreeNode(GNode n){
+        String packageName = n.getString(0);
+        String className =  n.getString(1);
+        return new TreeNode(packageName, className, n);
+    }
+
     public void simulateInheritance(){
         for(GNode headerAst:headerAsts){
+            TreeNode classTreeNode = createTreeNode(headerAst);
+
             if(headerAst.get(2) != null){
-                String extender = headerAst.getNode(2).getString(0);
-                String currPack = headerAst.getString(0);
-                Node extenderClass = getExtenderClass(extender, currPack);
+                Node extenderClass = getExtenderClass(
+                        headerAst.getNode(2).getString(0),
+                        headerAst.getString(0));
+                TreeNode extenderClassTreeNode = createTreeNode((GNode)extenderClass);
+                classTreeNode.extendsNodes.add(extenderClassTreeNode);
+            } else {
+                classTreeNode.extendsNodes = null;
             }
+            inheritanceTree.extendsNodes.add(classTreeNode);
         }
+
+        System.out.println(inheritanceTree.extendsNodes.toString());
+
     }
 
 
