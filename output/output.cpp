eafa9f609358d699b95d241e239cca353ed16508
Lina <lg1986@nyu.edu>
2017-04-24 18:46:45 -0400

    merge time

--- output/output.cpp
+++ output/output.cpp
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 #include "output.h"
 using namespace java::lang;
 namespace nyu{
 namespace edu{
 namespace oop{
-__A::__A() : __vptr(&__vtable) ,fld(__rt::literal("")){}
+__A::__A() : __vptr(&__vtable) ,self({}
 Class __A::__class() {
   static Class k = 
     new __Class(__rt::literal("nyu.edu.oop.A"), __Object::__class());
@@ -12,26 +11,21 @@ Class __A::__class() {
 }
 __A_VT __A::__vtable;
 A::__init(new__A(),(A __this ) { 
+self = ; 
 __Object::__init((Object)__this);
-A::__init(new__A(),(A __this , String fld)) { 
-ThisExpression(null) = fld__this;
 } 
 )
-String __A::getFld(A __this ) { 
-return ->__vptr->fld; 
-}
-__Test003::__Test003() : __vptr(&__vtable) {}
-Class __Test003::__class() {
+__Test009::__Test009() : __vptr(&__vtable) {}
+Class __Test009::__class() {
   static Class k = 
-    new __Class(__rt::literal("nyu.edu.oop.Test003"), __Object::__class());
+    new __Class(__rt::literal("nyu.edu.oop.Test009"), __Object::__class());
   return k;
 }
-__Test003_VT __Test003::__vtable;
+__Test009_VT __Test009::__vtable;
 int main(){ 
  a = A__::init(new __A(),); 
-cout <<(agetFld(a));
-
+cout <<(toString()); 
+} 
 }
 }
 }
-
