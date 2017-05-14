f84cc70a7aaddb3b621ba2ab0f14dbde8740d932
rishabhranawat <rishabhranawat12345@gmail.com>
2017-05-14 16:19:41 -0400

    merging

--- src/test/java/inputs/test001/Test001.java
+++ src/test/java/inputs/test001/Test001.java
@@ -2,34 +2,13 @@ package inputs.test001;
 
 class A {
     public String toString() {
-        String a = "A";
-        return a;
-    }
-    public A() {
-
-    }
-
-    public String myString(double i, int x) {
-        return "i";
-    }
-
-    public String myString(int x) {
-        return "s";
-    }
-
-}
-
-class B extends A {
-    public String myString(String s) {
-        return "S";
+        return "A";
     }
 }
 
 public class Test001 {
     public static void main(String[] args) {
         A a = new A();
-        String s = a.toString();
-        a.myString(1);
         System.out.println(a.toString());
     }
 }
